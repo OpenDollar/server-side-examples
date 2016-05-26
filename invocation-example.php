36fa822cb635f081907f92840868b8c04b054655
Oleksii Matiiasevych <lastperson@gmail.com>
2016-05-26 18:52:36 +0700

    Update examples.

--- invocation-example.php
+++ invocation-example.php
@@ -1,17 +1,17 @@
 <?php
 # get balance
-$address = '0x53786e5722f854a62783395dcdc27d633a9b063e';
+$address = '0xe176d196224fbf33d779f4a6275dcc31f3071f23';
 $balance = `node get-balance.js $address\n`;
 echo "balance: $balance";
 
 #send transaction to regular address
-$destination = '0x53786e5722f854a62783395dcdc27d633a9b063e';
+$destination = '0xe176d196224fbf33d779f4a6275dcc31f3071f23';
 $amount = 1.12;
 $txid = `node send-tx.js $destination`;
 echo "transaction hash: $txid";
 
 #send transaction to icap address
-$destination = 'XE33EXPXREG123456789';
+$destination = 'XE33ODLOPDL000000000';
 $amount = 1.12;
 $txid = `node send-tx.js $destination`;
 echo "second transaction hash: $txid";
--- invocation-example.rb
+++ invocation-example.rb
@@ -1,15 +1,15 @@
-balance = `node get-balance.js 0x53786e5722f854a62783395dcdc27d633a9b063e`;
+balance = `node get-balance.js 0xe176d196224fbf33d779f4a6275dcc31f3071f23`;
 print "balance: " + balance;
 
 #send transaction to regular address
-destination = '0x53786e5722f854a62783395dcdc27d633a9b063e';
+destination = '0xe176d196224fbf33d779f4a6275dcc31f3071f23';
 amount = 1.12;
 txid = `node send-tx.js #{destination} #{amount}`;
 print "transaction hash: #{txid}";
 
 
 #send transaction to ICAP address
-destination = 'XE33EXPXREG123456789';
+destination = 'XE33ODLOPDL000000000';
 amount = 1.12;
 txid = `node send-tx.js #{destination} #{amount}`;
 print "second transaction hash: #{txid}";
